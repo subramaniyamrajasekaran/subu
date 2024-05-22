bae98a34ec4aed070919d74fca763468a5a38344
subramaniyamrajasekaran <bharathi07101998@gmail.com>
2024-05-22 11:01:56 +0530

    first step

---
+++ src/org/sample/Types.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class Types {
+	public static void main(String[] args) {
+		String s="seleniumautomationtool";
+	    int a = s.indexOf('m');
+		System.out.println(a);
+		
+		String s1="j a v a p r o g r a m";
+		char b = s1.charAt(2);
+		System.out.println(b);
+		
+		String s3="Social media";
+		String s4="SocialMedia";
+		boolean c = s3.equalsIgnoreCase(s4);
+		System.out.println(c);
+		
+	
+	
+}
+}
+
